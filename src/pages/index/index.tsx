1a1a95457a5c3a676520f658b05de87c846428ce
ksora <ksora@live.com>
2025-06-18 22:48:55 +0800

    feat(index): draw count

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,35 @@
 import React, {useState} from 'react';
-import './index.less';
+import Cookie from 'js-cookie';
 import {useNavigate} from 'react-router-dom';
 import {ListData, Rules} from './constant';
 import Modal from '../../components/Modal';
 import {getInitialData, reportLog} from '../../public/service.ts';
+import './index.less';
 
-
+const initialCount = Cookie.get('count') ? parseInt(Cookie.get('count') || '0', 10) : 0;
 
 const IndexPage: React.FC = () => {
   const navigate = useNavigate();
   const [modalVisible, setModalVisible] = useState(false);
+  const [count, setCount] = useState(initialCount);
 
   const closeModal = () => {
     setModalVisible(false);
   };
 
   const handleDraw = () => {
-    reportLog('ACTIVITY_PLAY')
+    reportLog('ACTIVITY_PLAY');
     getInitialData().then(res => {
       if (res) {
+        setCount(prevCount => {
+          const newCount = prevCount + 1;
+          // 第二天 0 点
+          const expires = new Date();
+
+          expires.setHours(24, 0, 0, 0); // 设置为第二天的 0 点
+          Cookie.set('count', newCount.toString(), { expires });
+          return newCount;
+        });
         reportLog('ADVERT_SHOW', res.id);
         reportLog('ADVERT_CLICK', res.id);
         if (res.landpage.indexOf('/') === 0) {
@@ -43,7 +54,7 @@ const IndexPage: React.FC = () => {
       <div className={'index-water'}/>
 
       <div className={'index-count'}>
-        今日参与次数：6次
+        今日参与次数：{count}次
       </div>
 
       <div className={'index-rule'} onClick={() => setModalVisible(true)}>
